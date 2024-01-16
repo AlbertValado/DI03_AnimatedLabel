b468bcffbcf8d37f6af9c75e8b52e672ce4abaa1
Albert Valado <albertovalado@paucasesnovescifp.cat>
2024-01-16 21:55:29 +0100

    Se ha puesto todo en una única clase

--- AnimatedLabel/src/animatedlabel/LabelAnimatedLabel.java
+++ AnimatedLabel/src/animatedlabel/LabelAnimatedLabel.java
@@ -17,40 +17,74 @@ import javax.swing.Timer;
  */
 public class LabelAnimatedLabel extends JLabel implements Serializable {
 
-    private AnimatedLabel animatedLabel;
+    private String originalText;
+    private String appendedText;
+    private int delay;
+    private boolean animate;
     private Timer timer;
+
+    public LabelAnimatedLabel(String text, String appendedText, int delay, boolean animate) {
+        this.originalText = text;
+        this.appendedText = appendedText;
+        this.delay = delay;
+        this.animate = animate;
+    }
 
     public LabelAnimatedLabel() {
     }
 
-    public AnimatedLabel getAnimatedLabel() {
-        return animatedLabel;
+    public String getOriginalText() {
+        return originalText;
     }
 
-    public void setAnimatedLabel(AnimatedLabel animatedLabel) {
-        this.animatedLabel = animatedLabel;
+    public void setOriginalText(String text) {
+        this.originalText = text;
     }
+
+    public String getAppendedText() {
+        return appendedText;
+    }
+
+    public void setAppendedText(String appendedText) {
+        this.appendedText = appendedText;
+    }
+
+    public int getDelay() {
+        return delay;
+    }
+
+    public void setDelay(int delay) {
+        this.delay = delay;
+    }
+
+    public boolean isAnimate() {
+        return animate;
+    }
+
+    public void setAnimate(boolean animate) {
+        this.animate = animate;
+    }
+    
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (animatedLabel != null) {
-            timer = new Timer(animatedLabel.getDelay(), new ActionListener() {
+        
+            timer = new Timer(delay, new ActionListener() {
                 int i = 0;
 
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    if (animatedLabel.isAnimate()) {
-                        setText(animatedLabel.getText() + animatedLabel.getAppendedText().substring(0, i));
-                        i = (i + 1) % (animatedLabel.getAppendedText().length() + 1);
+                    if (animate) {
+                        setText(originalText + appendedText.substring(0, i));
+                        i = (i + 1) % (appendedText.length() + 1);
                     } else {
-                        setText(animatedLabel.getText());
+                        setText(originalText);
                     }
                 }
             });
             timer.start();
-        }
-
+        
     }
 
 }
